633512793da7b481bee449052ce8bc86b4605cac
Daniel Semler <47rooks@gmail.com>
2015-12-24 21:06:38 -0800

    remove unused imports

--- Uc2ool/src/ds/uc2ool/Uc2oolController.java
+++ Uc2ool/src/ds/uc2ool/Uc2oolController.java
@@ -1,8 +1,6 @@
 package ds.uc2ool;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.StringWriter;
 import java.net.URL;
 import java.util.Collections;
 import java.util.List;
